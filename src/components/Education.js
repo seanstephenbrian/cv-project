87a4fa8637535cf1a60f1296e9dc0178cf55412b
sean stephen brian <seanstephenbrian@gmail.com>
2023-01-23 12:19:01 -0500

    refactor Education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,118 +4,92 @@ import Add from '../components/Add';
 
 import '../styles/education.css';
 
-class Education extends React.Component {
+const Education = (props) => {
 
-    constructor(props) {
-        super(props);
+    const { educationInfo, editing } = props;
 
-        this.addEducationEntry = this.addEducationEntry.bind(this);
-        this.handleDeleteClick = this.handleDeleteClick.bind(this);
-        this.handleEditClick = this.handleEditClick.bind(this);
-        this.handleSaveEditsClick = this.handleSaveEditsClick.bind(this);
-        this.handleTextChange = this.handleTextChange.bind(this);
+    function addEducationEntry() {
+        props.createEducationEntry();
     }
 
-    addEducationEntry() {
-        this.props.createEducationEntry();
+    function handleDeleteClick(e) {
+        props.onDeleteClick(e);
     }
 
-    handleDeleteClick(e) {
-        this.props.onDeleteClick(e);
+    function handleEditClick() {
+        props.onEditEducationClick();
     }
 
-    handleEditClick() {
-        this.props.onEditEducationClick();
+    function handleSaveEditsClick() {
+        props.onSaveEditsClick();
     }
 
-    handleSaveEditsClick() {
-        this.props.onSaveEditsClick();
+    function handleTextChange(section, e) {
+        props.onTextChange(section, e.target.dataset.id, e.target.name, e.target.value);
     }
 
-    handleTextChange(section, e) {
-        this.props.onTextChange(section, e.target.dataset.id, e.target.name, e.target.value);
-    }
-
-    render() {
-        const { educationInfo, editing } = this.props;
-
-        // editing view:
-        if (editing) {
-            return (
-                <div className='education-section'>
-                    <div className='education-section-title'>
-                        <span className='education-title-border' style={{backgroundColor: this.props.accentColor}}>Education</span>
-                        <div className='edit-button edit-education no-print' onClick={this.handleSaveEditsClick}>Save All Edits</div>
-                    </div>
-
-                    {/* editable education entries: */}
-                    {educationInfo.map((entry) => {
-                        return (
-                            <div key={entry.id} className='editing-entry'>
-                                <div className='editing-entry-title'>
-                                    <span className='title-text'>Edit Item:</span>
-                                    <span className='delete-item' data-section='education' data-id={entry.id} onClick={this.handleDeleteClick}>Delete Item</span>
-                                </div>
-                                <div className='item-edits school-edits'>
-                                    <label className='edits-label' htmlFor='school'>School:</label>
-                                    <input className='edits-input' data-id={entry.id} type="text" name="school" value={entry.school} onChange={(e) => {this.handleTextChange('education', e)}} />
-                                </div>
-                                <div className='item-edits dates-edits'>
-                                    <label className='edits-label' htmlFor='dates'>Years of attendance:</label>
-                                    <input className='edits-input' data-id={entry.id} type='text' name='dates' value={entry.dates} onChange={(e) => {this.handleTextChange('education', e)}}></input>
-                                </div>
-                                <div className='item-edits degree-edits'>
-                                    <label className='edits-label' htmlFor='degree'>Degree/certification:</label>
-                                    <input className='edits-input' data-id={entry.id} type='text' name='degree' value={entry.degree} onChange={(e) => {this.handleTextChange('education', e)}}></input>
-                                </div>
-                                <div className='item-edits subject-edits'>
-                                    <label className='edits-label' htmlFor='subject'>Major/subject area:</label>
-                                    <input className='edits-input' data-id={entry.id} type='text' name='subject' value={entry.subject} onChange={(e) => {this.handleTextChange('education', e)}}></input>
-                                </div>
-                                <div className='item-edits additional-edits'>
-                                    <label className='edits-label' htmlFor='additional'>Additional information:</label>
-                                    <textarea className='edits-input' data-id={entry.id} name='additional' value={entry.additional} onChange={(e) => {this.handleTextChange('education', e)}}></textarea>
-                                </div>
-                                
-                            </div>
-                        )
-                    })}
-                    
-                    <Add onAddClick={this.addEducationEntry} section="education" />
-
-                </div>
-            )
-        }
-
-        // normal view:
+    // editing view:
+    if (editing) {
         return (
             <div className='education-section'>
                 <div className='education-section-title'>
-                    <span className='education-title-border' style={{backgroundColor: this.props.accentColor}}>Education</span>
-                    <div className='edit-button edit-education no-print' onClick={this.handleEditClick}>Edit</div>
+                    <span className='education-title-border' style={{backgroundColor: props.accentColor}}>Education</span>
+                    <div className='edit-button edit-education no-print' onClick={handleSaveEditsClick}>Save All Edits</div>
                 </div>
+
+                {/* editable education entries: */}
                 {educationInfo.map((entry) => {
-                    if (entry.subject) {
-                        return (
-                            <div className="education-entry" key={entry.id}>
-                                <div className='education-info'>
-                                    <h1 className='school-name'>{entry.school}</h1>
-                                    <h2 className='school-dates'>{entry.dates}</h2>
-                                    <h2 className='school-degree'>{entry.degree}, {entry.subject}</h2>
-                                </div>
-                                <div className='education-description'>
-                                    <h3 className='description-label'>Additional details:</h3>
-                                    <p className='description-text'>{entry.additional}</p>
-                                </div>
+                    return (
+                        <div key={entry.id} className='editing-entry'>
+                            <div className='editing-entry-title'>
+                                <span className='title-text'>Edit Item:</span>
+                                <span className='delete-item' data-section='education' data-id={entry.id} onClick={handleDeleteClick}>Delete Item</span>
                             </div>
-                        )
-                    }
+                            <div className='item-edits school-edits'>
+                                <label className='edits-label' htmlFor='school'>School:</label>
+                                <input className='edits-input' data-id={entry.id} type="text" name="school" value={entry.school} onChange={(e) => {handleTextChange('education', e)}} />
+                            </div>
+                            <div className='item-edits dates-edits'>
+                                <label className='edits-label' htmlFor='dates'>Years of attendance:</label>
+                                <input className='edits-input' data-id={entry.id} type='text' name='dates' value={entry.dates} onChange={(e) => {handleTextChange('education', e)}}></input>
+                            </div>
+                            <div className='item-edits degree-edits'>
+                                <label className='edits-label' htmlFor='degree'>Degree/certification:</label>
+                                <input className='edits-input' data-id={entry.id} type='text' name='degree' value={entry.degree} onChange={(e) => {handleTextChange('education', e)}}></input>
+                            </div>
+                            <div className='item-edits subject-edits'>
+                                <label className='edits-label' htmlFor='subject'>Major/subject area:</label>
+                                <input className='edits-input' data-id={entry.id} type='text' name='subject' value={entry.subject} onChange={(e) => {handleTextChange('education', e)}}></input>
+                            </div>
+                            <div className='item-edits additional-edits'>
+                                <label className='edits-label' htmlFor='additional'>Additional information:</label>
+                                <textarea className='edits-input' data-id={entry.id} name='additional' value={entry.additional} onChange={(e) => {handleTextChange('education', e)}}></textarea>
+                            </div>
+                            
+                        </div>
+                    )
+                })}
+                
+                <Add onAddClick={addEducationEntry} section="education" />
+            </div>
+        )
+    }
+
+    // default (non-editing) view:
+    return (
+        <div className='education-section'>
+            <div className='education-section-title'>
+                <span className='education-title-border' style={{backgroundColor: props.accentColor}}>Education</span>
+                <div className='edit-button edit-education no-print' onClick={handleEditClick}>Edit</div>
+            </div>
+            {educationInfo.map((entry) => {
+                if (entry.subject) {
                     return (
                         <div className="education-entry" key={entry.id}>
                             <div className='education-info'>
-                            <h1 className='school-name'>{entry.school}</h1>
-                                    <h2 className='school-dates'>{entry.dates}</h2>
-                                    <h2 className='school-degree'>{entry.degree}</h2>
+                                <h1 className='school-name'>{entry.school}</h1>
+                                <h2 className='school-dates'>{entry.dates}</h2>
+                                <h2 className='school-degree'>{entry.degree}, {entry.subject}</h2>
                             </div>
                             <div className='education-description'>
                                 <h3 className='description-label'>Additional details:</h3>
@@ -123,11 +97,25 @@ class Education extends React.Component {
                             </div>
                         </div>
                     )
-                })}
-                <Add onAddClick={this.addEducationEntry} section="education" />
-            </div>
-        )
-    }
-}
+                }
+                return (
+                    <div className="education-entry" key={entry.id}>
+                        <div className='education-info'>
+                        <h1 className='school-name'>{entry.school}</h1>
+                                <h2 className='school-dates'>{entry.dates}</h2>
+                                <h2 className='school-degree'>{entry.degree}</h2>
+                        </div>
+                        <div className='education-description'>
+                            <h3 className='description-label'>Additional details:</h3>
+                            <p className='description-text'>{entry.additional}</p>
+                        </div>
+                    </div>
+                )
+            })}
+
+            <Add onAddClick={addEducationEntry} section="education" />
+        </div>
+    )
+};
 
 export default Education;
