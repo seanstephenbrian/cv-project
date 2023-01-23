704e24e2c49a5f6373ffd92232070877e780431c
sean stephen brian <seanstephenbrian@gmail.com>
2023-01-23 12:25:31 -0500

    refactor Print component

--- src/components/Print.js
+++ src/components/Print.js
@@ -4,53 +4,44 @@ import PrintIcon from '../img/print.svg';
 
 import '../styles/print.css'
 
-class PrintOptions extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.handleColorChange = this.handleColorChange.bind(this);
-        this.handleFontClick = this.handleFontClick.bind(this);
+const PrintOptions = (props) => {
+    function handleColorChange(e) {
+        props.onColorChange(e.target.value);
     }
 
-    handleColorChange(e) {
-        this.props.onColorChange(e.target.value);
+    function handleFontClick(fontChoice) {
+        props.onFontClick(fontChoice);
     }
 
-    handleFontClick(fontChoice) {
-        this.props.onFontClick(fontChoice);
-    }
-
-    render() {
-        return (
-            <div className='print-options no-print'>
-                <div className='accent-options'>
-                    <label className='accent-text' htmlFor='accent-color'>Choose accent color:</label>
-                    <input className='color-picker' type='color' id='accent-color' name='accent-color' value={this.props.accentColor} onChange={this.handleColorChange} />
-                </div>
-                <div className='font-options'>
-                    <span className='font-text'>Choose font:</span>
-                    <div className={'font-list ' + this.props.selectedFont}>
-                        <span className='font-choice open-sans' onClick={() => this.handleFontClick('Open Sans')}>Open Sans</span>
-                        <span className='divider'>●</span>
-                        <span className='font-choice josefin-sans' onClick={() => this.handleFontClick('Josefin Sans')}>Josefin Sans</span>
-                        <span className='divider'>●</span>
-                        <span className='font-choice unbounded' onClick={() => this.handleFontClick('Unbounded')}>Unbounded</span>
-                        <span className='divider'>●</span>
-                        <span className='font-choice courier-prime' onClick={() => this.handleFontClick('Courier Prime')}>Courier Prime</span>
-                        <span className='divider'>●</span>
-                        <span className='font-choice eb-garamond' onClick={() => this.handleFontClick('EB Garamond')}>EB Garamond</span>
-                        <span className='divider'>●</span>
-                        <span className='font-choice torre-farfan' onClick={() => this.handleFontClick('Torre Farfan')}>Torre Farfan</span>
-                    </div>
-                </div>
-                <div className='print-section' onClick={window.print}>
-                    <span className='print-text'>Print Your CV</span>
-                    <img src={PrintIcon} alt='Print your CV' />
+    return (
+        <div className='print-options no-print'>
+            <div className='accent-options'>
+                <label className='accent-text' htmlFor='accent-color'>Choose accent color:</label>
+                <input className='color-picker' type='color' id='accent-color' name='accent-color' value={props.accentColor} onChange={handleColorChange} />
+            </div>
+            <div className='font-options'>
+                <span className='font-text'>Choose font:</span>
+                <div className={'font-list ' + props.selectedFont}>
+                    <span className='font-choice open-sans' onClick={() => handleFontClick('Open Sans')}>Open Sans</span>
+                    <span className='divider'>●</span>
+                    <span className='font-choice josefin-sans' onClick={() => handleFontClick('Josefin Sans')}>Josefin Sans</span>
+                    <span className='divider'>●</span>
+                    <span className='font-choice unbounded' onClick={() => handleFontClick('Unbounded')}>Unbounded</span>
+                    <span className='divider'>●</span>
+                    <span className='font-choice courier-prime' onClick={() => handleFontClick('Courier Prime')}>Courier Prime</span>
+                    <span className='divider'>●</span>
+                    <span className='font-choice eb-garamond' onClick={() => handleFontClick('EB Garamond')}>EB Garamond</span>
+                    <span className='divider'>●</span>
+                    <span className='font-choice torre-farfan' onClick={() => handleFontClick('Torre Farfan')}>Torre Farfan</span>
                 </div>
             </div>
-            
-        )
-    }
-}
+            <div className='print-section' onClick={window.print}>
+                <span className='print-text'>Print Your CV</span>
+                <img src={PrintIcon} alt='Print your CV' />
+            </div>
+        </div>
+        
+    )
+};
 
 export default PrintOptions;
