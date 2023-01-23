99098d11a1bf963d1ca70c2519b4533fc3c330f1
sean stephen brian <seanstephenbrian@gmail.com>
2023-01-23 12:20:32 -0500

    refactor Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,15 @@ import React, { useState } from 'react';
 
 import '../styles/footer.css';
 
-class Footer extends React.Component {
-    render() {
-        const year = new Date().getFullYear();
-        return (
-            <footer className='copyright no-print'>
-                <p>cv generator</p>
-                <p>copyright © {year}</p>
-                <p>ssbbd</p>
-            </footer>
-        )
-    }
+const Footer = () => {
+    const year = new Date().getFullYear();
+    return (
+        <footer className='copyright no-print'>
+            <p>cv generator</p>
+            <p>copyright © {year}</p>
+            <p>ssbbd</p>
+        </footer>
+    )
 }
 
 export default Footer;
