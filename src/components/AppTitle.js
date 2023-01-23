4ee76f6bf89135d134477014a69d964e9a031e16
sean stephen brian <seanstephenbrian@gmail.com>
2023-01-23 12:12:19 -0500

    refactor AppTitle component

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react';
 
 import '../styles/app-title.css';
 
-class AppTitle extends React.Component {
-    render() {
-        return (
-            <div className='app-title-container no-print'>
-                <span className='app-title-text'>CV Generator</span>
-            </div>
-        )
-    }
-}
+const AppTitle = (props) => {
+    return (
+        <div className='app-title-container no-print'>
+            <span className='app-title-text'>CV Generator</span>
+        </div>
+    )
+};
 
 export default AppTitle;
